f24bdec452264a53880a3a0a7a5cdac156f9330c
kauser1987 <kauser1987@gmail.com>
2024-05-26 18:26:05 +0530

    dotenv config

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,15 @@ app.use(express.json())
 connectDB();
 
 // app.use(cors())
-app.use(cors());
+// app.use(cors());
+app.use(cors(
+    {
+        origin: process.env.FRONTEND_URL,
+        credentials:true,
+    }
+))
+
+
 
 // api endpoints
 app.use("/api/user", userRouter)
