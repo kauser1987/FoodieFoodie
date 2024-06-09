e3ed906c965377822950f99af9e43f85c0a4f17f
kauser1987 <kauser1987@gmail.com>
2024-06-09 10:30:33 +0530

    changes

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -18,8 +18,8 @@ const StoreContextProvider = (props) => {
             else{
                 toast.error("Error")
               }
-        console.log(response.data.data)
-    }
+            }
+            console.log(response.data.data)
 
     const loadCartData = async (token) =>{
         const response = await axios.post(url+"/api/cart/get",{},{headers:{token}})
