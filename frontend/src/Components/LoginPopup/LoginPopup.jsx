fb7e48a390d2b37cd3d22fcc63ced2c0bfe1702b
kauser1987 <kauser1987@gmail.com>
2024-06-09 11:05:15 +0530

    minor

--- frontend/src/Components/LoginPopup/LoginPopup.jsx
+++ frontend/src/Components/LoginPopup/LoginPopup.jsx
@@ -29,7 +29,7 @@ const LoginPopup = ({setShowLogin}) => {
          const response = await axios.post(newUrl,data); 
             if(response.data.success){
                 setToken(response.data.token)
-                // localStorage.setItem("token",response.data.token)
+                localStorage.setItem("token",response.data.token)
                 setShowLogin(false)
             }
             else{
